0802f3104cb5a80e6eec55d4d1748f4d7533f9ae
Angella Lao <6369@holbertonstudents.com>
2023-10-19 17:39:06 +1100

    and delete api

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const appC = require("../controllers/AppController");
 const userC = require("../controllers/ProfileController");
 
+
 // link '/' to  AppController
 router.get("/", appC.getHomepage);
 
@@ -15,5 +16,8 @@ router.get("/profile/:id", userC.showUser);
 
 // Try: save data to database
 router.post("/profile", userC.saveToDatabase);
+
+// deletes user from database
+router.delete("/profile/:id", userC.deleteUser);
 
 module.exports = router;
