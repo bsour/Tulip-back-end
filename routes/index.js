5e2c7f244cbfc6016175a68e125120a09a05e6a1
Yu Ji <sophia.jiyu@gmai.com>
2023-10-23 15:05:25 +1100

    test to create a new user

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,46 @@ const express = require("express");
 const router = express.Router();
 const appC = require("../controllers/AppController");
 const userC = require("../controllers/ProfileController");
-const { userProfileValidation, handleValidationResult, userLogInValidation, handleLogInValidationResult } = require("./validationHelpers");
+const {
+  userProfileValidation,
+  handleValidationResult,
+  userLogInValidation,
+  handleLogInValidationResult,
+} = require("./validationHelpers");
 const auth = require("../middleware/auth");
 
-// link '/' to  AppController
+// get landing page
 router.get("/", appC.getHomepage);
 
-// link '/profile' to ProfileController
+// get all users
 router.get("/profile", userC.showUsers);
 
-// link '/profile'/:name to ProfileController
+// get a specific user
 router.get("/profile/:id", auth, userC.showUser);
 
-// Try: save data to database
-router.post("/profile", userProfileValidation, handleValidationResult, (req, res) => {
-  // If validation passes, continue to the route handler
-  console.log("User created!");
-  //userC.saveToDatabase(req, res);
-});
+// create new user
+router.post(
+  "/profile",
+  userProfileValidation,
+  handleValidationResult,
+  (req, res) => {
+    // If validation passes, continue to the route handler
+    console.log("User created!");
+    //userC.saveToDatabase(req, res);
+  }
+);
 
-// deletes user from database
+// delete a specific user
 router.delete("/profile/:id", userC.deleteUser);
 
-router.post("/auth", userLogInValidation, handleLogInValidationResult, (req, res) => {
-  console.log("logged in");
-});
+// what does this do?
+router.post(
+  "/auth",
+  userLogInValidation,
+  handleLogInValidationResult,
+  (req, res) => {
+    console.log("logged in");
+  }
+);
 
 module.exports = router;
