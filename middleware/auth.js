e7849a72df0cb7bbbbfed9c345290640ef6afb6c
Yu Ji <sophia.jiyu@gmai.com>
2023-10-24 12:21:27 +1100

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 const { body, validationResult } = require("express-validator");
-const bcrpyt = require("bcryptjs");
 
 module.exports = function (req, res, next) {
   // Get token from header
@@ -16,7 +15,7 @@ module.exports = function (req, res, next) {
   // Verify token
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-    req.user = decoded.user; // something to do with payload?
+    req.user = decoded.user; // assign user data extracted from the payload to authenticated request
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
