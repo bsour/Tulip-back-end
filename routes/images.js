6ce807099e29b4f1fe249a0274a7294dbe1f9ea1
Angella Lao <6369@holbertonstudents.com>
2023-10-30 23:07:29 +1100

    delete image from server after its sent to s3

--- routes/images.js
+++ routes/images.js
@@ -2,10 +2,14 @@ const express = require("express");
 const imageRouter = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
 
 const { uploadFile, getFileStream } = require("../s3");
 
 imageRouter.get("/:key", (req, res) => {
+  console.log(req.params);
   const key = req.params.key;
   const readStream = getFileStream(key);
 
@@ -16,6 +20,7 @@ imageRouter.post("/", upload.single("image"), async (req, res) => {
   const file = req.file;
   console.log(file);
   const result = await uploadFile(file);
+  await unlinkFile(file.path)
   console.log(result);
   const description = req.body.description;
   res.send({ imagePath: `/uploads/${result.Key}` });
